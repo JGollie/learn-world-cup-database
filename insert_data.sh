13504d907a64cd4b5f4c378539a8306d82143fe2
JGollie <jonathan.lgolden@gmail.com>
2024-04-01 18:42:40 -0400

    only queries left

--- insert_data.sh
+++ insert_data.sh
@@ -12,7 +12,7 @@ echo $($PSQL "TRUNCATE games, teams")
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
 # get winning teams name from CSV
-  if [[ $WINNER != name ]]
+  if [[ $WINNER != winner ]]
   then
     # get winner ID    
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name ='$WINNER'")    
@@ -21,14 +21,14 @@ do
     if [[ -z $WINNER_ID ]]
     then
     INSERT_TEAM_NAME=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-      if [[ $WINNER_ID == 'INSERT O 1' ]] 
+      if [[ $INSERT_TEAM_NAME == 'INSERT O 1' ]] 
       then
         echo -e "\nInserted into teams: '$WINNER'"
       fi
     fi
   fi
 # get losing teams name from CSV
-  if [[ $OPPONENT != name ]]
+  if [[ $OPPONENT != opponent ]]
   then
     # get opponent ID
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name ='$OPPONENT'")
@@ -37,11 +37,33 @@ do
     if [[ -z $OPPONENT_ID ]]
     then 
     INSERT_OPPONENT_ID=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-      if [[ $OPPONENT == 'INSERT 0 1' ]]
+      if [[ $INSERT_OPPONENT_ID == 'INSERT 0 1' ]]
       then 
         echo -e "\nInserted into teams: '$OPPONENT'"
       fi
     fi
   fi
 done
+
 # get game information from CSV
+echo $($PSQL "TRUNCATE year, round, winner_goals, opponent_goals")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+
+do
+  if [[ $YEAR != year ]]
+  then
+    # get winner ID
+    WINNER_IDD=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
+     
+    # get opponent ID
+    OPPONENT_IDD=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
+      
+    # insert game information from csv
+    INSERT_GAME_INFO=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR', '$ROUND', '$WINNER_IDD', '$OPPONENT_IDD', '$WINNER_GOALS', '$OPPONENT_GOALS')")
+      #if [[ $INSERT_GAME_INFO = 'INSERT 0 1' ]]
+        echo -e "\nAdded the $YEAR game between '$WINNER_IDD' and '$OPPONENT_IDD':"
+      #fi
+    
+  fi
+ 
+done

fdbffa97fbcfb47296d697b5f83f47603c60fd62
JGollie <jonathan.lgolden@gmail.com>
2024-04-02 16:05:21 -0400

    Save progress: 1.1

--- queries.sh
+++ queries.sh
@@ -32,10 +32,10 @@ echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
 echo "$($PSQL "SELECT name FROM (SELECT winner_id AS team_id FROM games WHERE year = 2014 UNION SELECT opponent_id AS team_id FROM games WHERE year = 2014) AS played_teams JOIN teams on played_teams.team_id = teams.team_id ORDER BY name;")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT teams.name FROM games INNER JOIN teams ON games.winner_id = teams.team_id GROUP BY teams.name ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT year, teams.name FROM games INNER JOIN teams ON games.winner_id = teams.team_id WHERE round = 'Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo "$($PSQL "SELECT teams.name FROM games INNER JOIN teams ON games.winner_id = teams.team_id WHERE name LIKE 'Co%'")"
